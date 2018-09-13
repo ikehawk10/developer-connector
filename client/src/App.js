3a29da763a9e0048a92dcc169df47770a158fb67
Isaac <hawkins.isaac@gmail.com>
2018-09-13 14:49:25 -0400

    add redux actions and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,11 @@ import { Provider } from 'react-redux';
 
 import store from './store';
 
-
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-
 
 import './App.css';
 
