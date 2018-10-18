a22b35ab9ba3126d93c46b17ab1c4c8811595ff1
Isaac <hawkins.isaac@gmail.com>
2018-10-18 12:53:59 -0500

    add spinner component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 		return (
 			<div>
 				<footer className="bg-dark text-white mt-5 p-4 text-center">
-    		Copyright &copy; {new Date().getFullYear()} 2018 DevConnector
+    		Copyright &copy; {new Date().getFullYear()} DevConnector
   			</footer>
   		</div>
 		)
