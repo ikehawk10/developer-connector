5b16f5dec752916c8182be0bbd73ef9688e4e7fe
Isaac <hawkins.isaac@gmail.com>
2018-11-14 22:37:45 -0600

    add delete functionality of experience

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Moment from 'react-moment';
+import { Moment } from 'react-moment';
 import { deleteEducation } from '../../actions/profileActions';
 
 class Education extends Component {
